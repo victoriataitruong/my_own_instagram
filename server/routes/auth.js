3992d3a00909f2baa2eb13ee127ee0b05fcd5e06
Victoria Truong <victoriataitruong@gmail.com>
2021-04-29 13:39:24 -0700

    adding ig post to rest client

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,9 +6,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../keys')
 const requireLogin = require('../middleware/requireLogin')
-router.get('/protected', requireLogin, (req, res) => {
-	res.send('hello user')
-})
+
 router.post('/signup', (req, res) => {
 	const { name, email, password } = req.body
 	if (!name || !email || !password) {
